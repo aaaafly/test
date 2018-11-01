1494f7706009f1d54a3eadae786acbb0b65a6e16
aaaafly <38147240+aaaafly@users.noreply.github.com>
2018-11-01 14:06:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,msg)
 #------------------------------------------------------------------------------------------------------#
     elif(get.find('柚子抽') >= 0):
-        score = ma_score(get[3:])
+        score = 'test'
         
         msg = TextSendMessage(score)
         
