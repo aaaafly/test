c66d528a08c0dced59aff79e65831d8e4acdb02a
aaaafly <38147240+aaaafly@users.noreply.github.com>
2019-03-11 15:47:57 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
-import schedule
-import time
-import random
 
-from linebot import LineBotApi
-from linebot.models import *
+
+#----------------line bot api----------------#
+
+from flask import Flask, request, abort
 from imgurpython import ImgurClient
 
-import requests
-import json
-import re
-from bs4 import BeautifulSoup as bs
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+import tempfile,os
+
+app = Flask(__name__)
+
+#----------------ACCESS_TOKEN----------------#
+
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+SECRET = os.environ.get('SECRET')
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
-import os
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # get user id when reply
+    user_id = event.source.user_id
+    group_id = event.source.group_id
+    print("user_id =", user_id)
+    print("group_id = ", group_id)
 
-secrets = 'e38e393f3a88e64d2d2c49a556fcac5c'
-channel_access_token = 'dy6ze9aF7gFIwzqoflHoPRV/Z8aO/NpWHswJt8Tzj02dIISCVDRZeAprehpfVzpvMX0grDAgycZBaT3WadkxhQeS3ZpMRTZTTaggufUGa4XWf5xIPWz/dJ9SOKI6R7hAqazmeJDXXJzLTb3LFOpRZwdB04t89/1O/w1cDnyilFU='
-your_id='Uf5cf17f090a2dca6b8631ee06816306a'
-
-line_bot_api = LineBotApi(channel_access_token)
-
-def push():
-    print("working at")
-    print(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
-
-    msg1 = TextSendMessage("%Y-%m-%d %H:%M:%S\n", time.localtime())
-    line_bot_api.push_message(your_id,messages=msg1)
-    msg2 = ImageSendMessage(original_content_url=https://www.instagram.com/p/BtvENlGHLnN/media/?size=l,preview_image_url=https://www.instagram.com/p/BtvENlGHLnN/media/?size=l)
-    line_bot_api.push_message(your_id,messages=msg2)
-    
-  
-schedule.every().hour.do(push)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    app.run()
