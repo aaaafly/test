6109f97b919818ad127a185046187d85101a2419
aaaafly <38147240+aaaafly@users.noreply.github.com>
2019-03-11 15:55:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -48,8 +48,10 @@ def handle_message(event):
     # get user id when reply
     user_id = event.source.user_id
     group_id = event.source.group_id
-    print("user_id =", user_id)
-    print("group_id = ", group_id)
+    
+    msg = "user_id =" + user_id + "\ngroup_id = " + group_id
 
+    line_bot_api.reply_message(event.reply_token,msg)
+    
 if __name__ == "__main__":
     app.run()
